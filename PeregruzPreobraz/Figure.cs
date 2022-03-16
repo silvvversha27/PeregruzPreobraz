cd133fca74b5499ced898f52fc4b02dba9c7e2f9
User <User@DESKTOP-C7DGT0E>
2022-03-16 17:25:58 +0200

    Добавьте файлы проекта.

---
+++ PeregruzPreobraz/Figure.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeregruzPreobraz
+{
+    abstract class Figure
+    {
+        public abstract void Draw();
+    }
+    abstract class Quadrangle : Figure { }
+    class Rectangle:Quadrangle 
+    {
+     public int Width { get; set; }
+     public int Height { get; set; }
+        public static implicit operator Rectangle(Square s)
+        {
+            return new Rectangle { Width=s.Length*2, Height=s.Length };
+        }
+        public override void Draw()
+        {
+            for (int i = 0; i < Height; i++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public override string? ToString()
+        {
+            return $"Rectangle: width={Width}, height={Height}";
+        }
+    }
+    class Square : Quadrangle
+    {
+        public int Length { get; set; }
+        public static explicit operator Square(Rectangle rect)
+        {
+            return new Square { Length = rect.Height };
+        }
+        public static explicit operator int(Square rect)
+        {
+            return rect.Length;
+        }
+        public static implicit operator Square(int number)
+        {
+            return new Square { Length = number };
+        }
+
+        public override void Draw()
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                for (int j = 0; j < Length; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        public override string? ToString()
+        {
+            return $"Square: length={Length}";
+        }
+    }
+}
---
+++ PeregruzPreobraz/Program.cs
@@ -0,0 +1,18 @@
+﻿using PeregruzPreobraz;
+
+Rectangle rectangle = new Rectangle { Width = 5, Height = 10 };
+Square square = new Square {Length=7};
+
+rectangle.Draw();
+square.Draw();
+
+Rectangle rectSquare = square;
+rectangle.Draw();
+
+Square squareRect = (Square)rectangle;
+squareRect.Draw();
+int number=int.Parse(Console.ReadLine());
+Square squareInt = number;
+squareInt.Draw();
+number = (int)square;
+Console.WriteLine(number);
